c336c4d7c4a34892abfd6b54f042e882ec9f2812
Amr Emara <a.emara47@gmail.com>
2021-10-25 08:10:17 -0400

    Add overview-structure-surface toggle component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Components/Header';
+import StructureSurfaceToggle from './Components/StructureSurfaceToggle';
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <StructureSurfaceToggle/>
     </div>
   );
 }
---
+++ src/Components/StructureSurfaceToggle.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const MobileToggle = styled.div`
+  display: flex;
+  border-bottom: 1px solid rgba(151, 151, 151, 0.1);
+  padding: 1.5em 2em 0 2em;
+  justify-content: space-between;
+`;
+
+const ToggleButton = styled.button`
+  font-family: "Spartan", sans-serif;
+  font-size: 0.56rem;
+  text-transform: uppercase;
+  letter-spacing: 1.93px;
+  font-weight: 700;
+  border: none;
+  padding: 0 .3em 2em .3em;
+  background: none;
+  color: #ffffff;
+  border-bottom: 4px solid rgba(65, 158, 187, 0);
+
+  &:hover {
+    cursor: pointer;
+    border-bottom: 4px solid rgba(65, 158, 187, 1);
+  }
+`;
+
+const StructureSurfaceToggle = () => {
+    return (
+      <MobileToggle>
+        <ToggleButton>overview</ToggleButton>
+        <ToggleButton>structure</ToggleButton>
+        <ToggleButton>surface</ToggleButton>
+      </MobileToggle>
+    );
+}
+
+export default StructureSurfaceToggle

44b9b61c16d64ef57f6cd7676751521ff76dc8c1
Amr Emara <a.emara47@gmail.com>
2021-10-25 21:34:01 -0400

    Add states to main app component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import StructureSurfaceToggle from './Components/StructureSurfaceToggle';
 import styled from 'styled-components';
 import PlanetDescription  from './Components/PlanetDescription';
 import QuantativeInfo from './Components/QuantativeInfo';
+import data from './data.json';
+import { useState ,useEffect} from 'react';
+
 
 const MainContainer = styled.main`
   display: flex;
@@ -12,14 +15,35 @@ const MainContainer = styled.main`
 `;
 
 function App() {
+  const [planet, setPlanet] = useState('Mercury');
+  const [aspect, setAspect] = useState('overview');
+  const [planetData, setPlanetData] = useState(data[0]);
+
+  const changePlanet = (planet) => {
+    setPlanet(planet);
+  }
+
+  const changeAspect = (aspect) => {
+    setAspect(aspect);
+  }
+
+   useEffect(() => {
+    data.forEach((planetObject) => {
+      if(planetObject.name === planet) {
+        setPlanetData(planetObject)
+      }
+    })
+    
+  }, [planet])
+
   return (
     <div className="App">
-      <Header />
-      <StructureSurfaceToggle />
+      <Header planet={planet} changePlanet={changePlanet} />
+      <StructureSurfaceToggle aspect={aspect} changeAspect={changeAspect} />
       <MainContainer>
-        <PlanetImage />
-        <PlanetDescription />
-        <QuantativeInfo />
+        <PlanetImage planetData={planetData} aspect={aspect} />
+        <PlanetDescription planetData={planetData} aspect={aspect} />
+        <QuantativeInfo planetData={planetData} />
       </MainContainer>
     </div>
   );
